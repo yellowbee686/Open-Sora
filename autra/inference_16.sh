eda422d3cdce781c15aab23ccf5488364cc87b99
ChengXiao686 <xiao.cheng@autra.tech>
2024-03-21 20:14:13 +0800

    debug

--- autra/inference_16.sh
+++ autra/inference_16.sh
@@ -1,1 +1,1 @@
-torchrun --standalone --nproc_per_node 1 scripts/inference.py configs/opensora/inference/16x512x512.py --ckpt-path OpenSora-v1-HQ-16x512x512.pth --prompt-path ./assets/texts/t2v_samples.txt
+torchrun --standalone --nproc_per_node 1 scripts/inference.py configs/opensora/inference/16x512x512.py --ckpt-path OpenSora-v1-HQ-16x512x512.pth
