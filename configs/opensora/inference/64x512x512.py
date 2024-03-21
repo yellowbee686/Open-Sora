644234e9601441eaf629ab229a2c0cbdc9ceb03f
ChengXiao686 <xiao.cheng@autra.tech>
2024-03-21 20:04:01 +0800

    update text_encoder

--- configs/opensora/inference/64x512x512.py
+++ configs/opensora/inference/64x512x512.py
@@ -18,7 +18,7 @@ vae = dict(
 )
 text_encoder = dict(
     type="t5",
-    from_pretrained="DeepFloyd/t5-v1_1-xxl",
+    from_pretrained="DeepFloyd/",
     model_max_length=120,
 )
 scheduler = dict(
